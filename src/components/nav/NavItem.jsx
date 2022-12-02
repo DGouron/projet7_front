5101465bdadcb75446d360ac0124f413a557c34c
dgouron <damien.gouron@gmail.com>
2022-12-02 14:20:35 +0100

    Update NavItem.jsx

--- src/components/nav/NavItem.jsx
+++ src/components/nav/NavItem.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./Nav.module.css";
 
 function NavItem({ url, title }) {
+  // This styling will be applied to a <NavLink> when the
+  // route that it links to is currently selected.
+  let activeStyle = {
+    textDecoration: "underline",
+  };
+
+  let unactiveStyle = {
+    textDecoration: "none",
+  };
+
   return (
-    <Link to={url} className={styles.nav__item}>
+    <NavLink
+      to={url}
+      className={styles.nav__item}
+      style={({ isActive }) => (isActive ? activeStyle : unactiveStyle)}
+    >
       {title}
-    </Link>
+    </NavLink>
   );
 }
 
