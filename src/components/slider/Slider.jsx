181a0a02ad67eb88865afb69264439953a76d3e4
dgouron <damien.gouron@gmail.com>
2023-01-20 14:44:52 +0100

    fix some problems :)

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,10 +3,8 @@ import styles from "./Slider.module.css";
 import arrow from "../../assets/VectorArrow.svg";
 
 function Slider({ pictures }) {
-  // Définissez un état pour enregistrer l'index de l'image actuellement affichée
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // Définissez des fonctions pour passer à l'image suivante ou précédente
   const previous = () => {
     const newIndex = currentIndex - 1;
     setCurrentIndex(newIndex < 0 ? pictures.length - 1 : newIndex);
@@ -19,7 +17,6 @@ function Slider({ pictures }) {
 
   return (
     <section className={styles.slider__container}>
-      {/* Affichez l'image actuelle */}
       <div className={styles.slide__wrapper}>
         <img
           src={pictures[currentIndex]}
@@ -36,14 +33,12 @@ function Slider({ pictures }) {
               alt="arrow"
               onClick={previous}
               className={styles.slider__previousarrow}
-              style={{ display: "inline-block", margin: "0 auto" }}
             />
             <img
               src={arrow}
               alt="arrow"
               onClick={next}
               className={styles.slider__nextarrow}
-              style={{ display: "inline-block", margin: "0 auto" }}
             />
           </>
         ) : null}
